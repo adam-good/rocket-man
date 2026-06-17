26f104b9898458417a0089d3309b22c0cfef9c6d
Adam Good <dev.adamgood@gmail.com>
2026-06-17 15:07:57 -0400

    feature: write to csv

--- physics.clj
+++ physics.clj
@@ -27,30 +27,34 @@
     (update-vector (:acceleration object) jerk delta-time)
     (:mass object))))
 
-(def target (v3/->Vector3 0 10 0))
-(defn jerk [pos targ] 
-  (->> (v3/elem-subtract targ pos) (v3/elem3-op #(min 1 %)) ))
-(def obj 
-  (->PhysicalObj 
+(def target (v3/->Vector3 0 100 0))
+(defn jerk [pos targ]
+  (->> (v3/elem-subtract targ pos) (v3/elem3-op #(min 1 %))))
+(def obj
+  (->PhysicalObj
    (v3/zero) ; Position 
    (v3/zero) ; Velocity 
    (v3/->Vector3 5 0 2.5) ; Acceleration 
    1))
 (def dt 0.05)
 (def time-series (iterate #(+ dt %) 0.0))
-(def obj-series 
+(def obj-series
   (iterate #(update-obj % (-> (:position %) (jerk target)) dt) obj))
 (def raw-data
   (utils/zip time-series (for [obj obj-series] obj)))
 
-;; (require '[clojure.data.csv :as csv] '[clojure.java.io :as io])
-;; (def csv-data 
-;;   (for [datapoint raw-data] (merge {:time (first datapoint)} (second datapoint))))
-;; 
-;; (defn write-csv [path data]
-;;   (let [columns [:time :x :y :z]
-;;         headers (map name columns)
-;;         rows (mapv #(mapv % columns) data)]
-;;     (with-open [file (io/write)])))
-;; 
-;; (take 10 csv-data)
+;; Write to CSV
+(require '[clojure.java.io :as io] '[clojure.string :refer [join]])
+(def csv-data
+  (for [datapoint raw-data] (merge {:time (first datapoint)} (second datapoint))))
+
+
+(def csv-header (->> [:time :x :y :z] (map name) (join ",")))
+(defn csv-row [{t :time {x :x y :y z :z} :position}] (join "," [t x y z]))
+(def csv-row-data (map csv-row csv-data))
+
+(defn write-csv [nrows path]
+  (with-open [file (io/writer path)]
+    (.write file (->> csv-row-data (take nrows) (cons csv-header) (join "\n")))))
+
+(write-csv 100 "./test.csv")
