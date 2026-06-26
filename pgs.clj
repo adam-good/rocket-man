c8d5fd5a2a2a91dd21b4266dcd726cfd291006d1
Adam Good <dev.adamgood@gmail.com>
2026-06-26 15:26:29 -0400

    feature: calculate-jerk still broken

--- pgs.clj
+++ pgs.clj
@@ -10,34 +10,28 @@
 
 ;; Helper Functions
 (defn distance [u v] (->> (v3/elem-subtract v u) (v3/magnitude) (abs)))
-(defn diff-direction [u v] (->> (v3/elem-subtract u v) (v3/normalize)))
 (defn impact? [projectile target] (->> (:position projectile) (distance target) (< 1e-5)))
 
-(defn calculate-jerk-magnitude
-  [dist speed]
-  (/ 2 1))
-
-(defn calculate-jerk-direction
-  [targ-dir vel-dir acc-dir delta-t]
-  (let [dt1 (/ 1 delta-t) dt2 (* dt1 dt1)]
-    (v3/elem-add
-     (->> (v3/elem-subtract vel-dir targ-dir) (v3/scalar-product dt2))
-     (v3/scalar-product dt1 acc-dir))))
-
-
-;; Projectile Guidance System
 (defn calculate-jerk 
   "Projectile Guidance System (PGS)\n
    Calculates the needed Jerk to guide the projectile to the target"
-  [direction magnitude] 
-  (v3/scalar-product magnitude direction))
+  [pos vel acc targ delta-t]
+  (let [pos-diff (v3/elem-subtract pos targ)
+        dt  (/ 1 delta-t)
+        dt2 (/ 1 (* delta-t delta-t))
+        dt3 (/ 1 (* delta-t delta-t delta-t))]
+    (v3/elem-add
+     (v3/scalar-product (* -2 dt3) pos-diff)
+     (v3/scalar-product dt3 vel)
+     (v3/scalar-product dt2 vel)
+     (v3/scalar-product dt acc))))
 
 ;; Initial Conditions
 (def target (v3/->Vector3 1 1 1))
 (def projectile
   (phi/->PhysicalObj
    (v3/zero)             ; Position 
-   (v3/->Vector3 0 0 0)  ; Velocity 
+   (v3/->Vector3 0 0 1)  ; Velocity 
    (v3/zero)             ; Acceleration 
    1))
 (def dt 0.05)
@@ -47,13 +41,7 @@
 (def obj-series
   (iterate 
    #(phi/update-obj 
-     % (calculate-jerk 
-        (calculate-jerk-direction 
-         (diff-direction (:position %) target) 
-         (v3/normalize (:velocity %)) 
-         (v3/normalize (:acceleration %))
-         dt)
-        (calculate-jerk-magnitude (distance (:position %) target) (v3/magnitude (:velocity %))))
+     % (calculate-jerk (:position %) (:velocity %) (:acceleration %) target dt)
      dt) 
    projectile ))
 
