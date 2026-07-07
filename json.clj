d9d5553f37da47ba483b93d424f5b642c60dd05a
Adam Good <dev.adamgood@gmail.com>
2026-07-07 16:39:54 -0400

    wip: json

--- json.clj
+++ json.clj
@@ -2,3 +2,21 @@
   (:require
    [clojure.java.io :as io]
    [clojure.string :refer [join]]))
+
+(derive java.util.Map ::collection)
+
+(defn key-to-string [key]
+  (str (name key) ":"))
+
+(defmulti to-json class)
+(defmethod to-json :default      [data] (str data))
+(defmethod to-json ::collection  [data]
+  (->>
+   (for [[key value] data] (str "{" (key-to-string key) (to-json value) "}"))
+   (join ",")))
+
+
+(def data {:a "A" :b {:c "C"}})
+(to-json data)
+
+
