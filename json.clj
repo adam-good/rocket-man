4584071bbebd6695d495181a69e10633e9f39772
Adam Good <dev.adamgood@gmail.com>
2026-07-08 15:17:38 -0400

    feature: vec/map to json

--- json.clj
+++ json.clj
@@ -4,19 +4,36 @@
    [clojure.string :refer [join]]))
 
 (derive java.util.Map ::collection)
+(derive java.util.ArrayList ::array)
 
 (defn key-to-string [key]
   (str (name key) ":"))
 
+(defn create-json-pair [key value]
+  (str (key-to-string key) value))
+
+(defn bracketize-curly [s] (str "{" s "}"))
+(defn bracketize-square [s] (str "[" s "]"))
+
 (defmulti to-json class)
 (defmethod to-json :default      [data] (str data))
+(defmethod to-json ::array       [data] 
+  (->>
+   (map to-json data)
+   (join ",")
+   (bracketize-square)))
 (defmethod to-json ::collection  [data]
   (->>
-   (for [[key value] data] (str "{" (key-to-string key) (to-json value) "}"))
-   (join ",")))
+   (for [[key value] data]
+     (create-json-pair key (to-json value)))
+   (join ",")
+   (bracketize-curly)))
 
 
-(def data {:a "A" :b {:c "C"}})
+(def data
+  [
+   {:a "A" :b "B" :c {:d "D"}}
+   {:e "E" :f {:g "G"} :h "H"}
+] )
 (to-json data)
-
 
