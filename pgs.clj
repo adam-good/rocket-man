d1b1a783476f0d772cb29bc14d79ab7c1cdf4d9c
Adam Good <dev.adamgood@gmail.com>
2026-07-29 14:32:52 -0400

    wip: more pgs function dispatch

--- pgs.clj
+++ pgs.clj
@@ -32,20 +32,27 @@
    (contains? constraints :vt)
    (contains? constraints :at)))
 
-(defn constraint-dispatch [constraints]
+(defn constraint-dispatch [_N constraints]
   (cond
     (target-rank1? constraints) ::rank1
     (target-rank2? constraints) ::rank2
     (target-rank3? constraints) ::rank3))
 
-(defmulti c0 constraint-dispatch)
-(defmethod c0 :default [_N {r0 :r0}] r0)
+;; TODO: This might be simplified after I solve other cases
+(defmulti  c0 constraint-dispatch)
+(defmethod c0 ::rank1 [_N {r0 :r0}] r0)
+(defmethod c0 ::rank2 [_N {_r0 :r0}] :unimplemented)
+(defmethod c0 ::rank3 [_N {r0 :r0}] r0)
 
-(defmulti c1 constraint-dispatch)
-(defmethod c1 :default [_N {v0 :v0}] v0)
+(defmulti  c1 constraint-dispatch)
+(defmethod c1 ::rank1 [_N {v0 :v0}] v0)
+(defmethod c1 ::rank2 [_N {_v0 :v0}] :unimplemented)
+(defmethod c1 ::rank3 [_N {v0 :v0}] v0)
 
-(defmulti c2 constraint-dispatch)
-(defmethod c2 :default [_N {a0 :a0}] (/ a0 2))
+(defmulti  c2 constraint-dispatch)
+(defmethod c2 ::rank1 [_N {a0 :a0}] (/ a0 2))
+(defmethod c2 ::rank2 [_N {_a0 :a0}] :unimplemented)
+(defmethod c2 ::rank3 [_N {a0 :a0}] (/ a0 2))
 
 (defmulti c3 constraint-dispatch)
 (defmethod c3 ::rank1
@@ -74,104 +81,104 @@
      (v3/scalar-product vel-coef vel-diff)
      (v3/scalar-product acc-coef acc-diff))))
 
-; (defn c3
-;   ([N r0 v0 a0 rt]
-;    (let [pos-coef (/ 1 (* N N N))
-;          vel-coef (/ 1 (* N N))
-;          acc-coef (/ 1 N)
-;          position (v3/elem-subtract rt r0)]
-;      (v3/elem-add
-;       (v3/scalar-product pos-coef position)
-;       (v3/scalar-product (* -1 vel-coef) v0)
-;       (v3/scalar-product (* -1 acc-coef) a0))))
-;   ([N r0 v0 a0 rt vt at]
-;    (let [pos-coef (/ 1 (* N N N))
-;          vel-coef (/ 1 (* N N))
-;          acc-coef (/ 1 (* 2 N))
-;          position (v3/elem-add (v3/scalar-product 10 rt) (v3/scalar-product -10 r0))
-;          velocity (v3/elem-add (v3/scalar-product -9 vt) (v3/scalar-product  -6 v0))
-;          accel    (v3/elem-add  at                       (v3/scalar-product  -3 a0))]
-;      (v3/elem-add
-;       (v3/scalar-product pos-coef position)
-;       (v3/scalar-product vel-coef velocity)
-;       (v3/scalar-product acc-coef accel)))))
-
-(defn c4 [N r0 rt v0 vt a0 at]
-  (let [pos-coef (/ 15 (* N N N N))
-        vel-coef (/ 1 (* N N N))
-        acc-coef (/ 1 (* 2 N N))
-        position (v3/elem-subtract r0 rt)
-        velocity (v3/elem-add (v3/scalar-product 8 v0) (v3/scalar-product  7 vt))
-        accel    (v3/elem-add (v3/scalar-product 3 a0) (v3/scalar-product -2 at))]
+(defmulti  c4 constraint-dispatch)
+(defmethod c4 ::rank1 [_N {}] 0) ; Rank 1 uses a rank3 polynomial
+(defmethod c4 ::rank2 [_N {}] :unimplimented)
+(defmethod c4 ::rank3
+  [N {r0 :r0 v0 :v0 a0 :a0 rt :rt vt :vt at :at}]
+  (let [pos-coef (/ 1 N N N N)
+        vel-coef (/ 1 N N N)
+        acc-coef (/ 1 2 N N)
+        pos-diff (v3/elem-add (v3/scalar-product 15 r0) (v3/scalar-product -15 rt))
+        vel-diff (v3/elem-add (v3/scalar-product 8 v0) (v3/scalar-product 7 vt))
+        acc-diff (v3/elem-add (v3/scalar-product 3 a0) (v3/scalar-product -2 at))]
     (v3/elem-add
-     (v3/scalar-product pos-coef position)
-     (v3/scalar-product vel-coef velocity)
-     (v3/scalar-product acc-coef accel))))
+     (v3/scalar-product pos-coef pos-diff)
+     (v3/scalar-product vel-coef vel-diff)
+     (v3/scalar-product acc-coef acc-diff))))
 
-(defn c5 [N r0 rt v0 vt a0 at]
-  (let [pos-coef (/  6 (* N N N N N))
-        vel-coef (/ -3 (* N N N N))
-        acc-coef (/  1 (* N N))
-        position (v3/elem-subtract rt r0)
-        velocity (v3/elem-add v0 vt)
-        accel    (v3/elem-subtract at a0)]
+(defmulti  c5 constraint-dispatch)
+(defmethod c5 ::rank1 [_N _map] 0)
+(defmethod c5 ::rank2 [_N _map] :unimplemented)
+(defmethod c5 ::rank3
+  [N {r0 :r0 v0 :v0 a0 :a0 rt :rt vt :vt at :at}]
+  (let [pos-coef (/  6 N N N N N)
+        vel-coef (/ -3 N N N N)
+        acc-coef (/  1 N N)
+        pos-diff (v3/elem-subtract rt r0)
+        vel-diff (v3/elem-add v0 vt)
+        acc-diff (v3/elem-subtract at a0)]
     (v3/elem-add
-     (v3/scalar-product pos-coef position)
-     (v3/scalar-product vel-coef velocity)
-     (v3/scalar-product acc-coef accel))))
+     (v3/scalar-product pos-coef pos-diff)
+     (v3/scalar-product vel-coef vel-diff)
+     (v3/scalar-product acc-coef acc-diff))))
 
-(defn jerk-profile
-  ([N r0 v0 a0 rt]
-   (v3/scalar-product 6 (c3 N r0 v0 a0 rt)))
-  ([N r0 v0 a0 rt vt at]
-   (v3/elem-add
-    (v3/scalar-product 6 (c3 N r0 rt v0 vt a0 at))
-    (v3/scalar-product (* 24 N) (c4 N r0 rt v0 vt a0 at))
-    (v3/scalar-product (* 60 N N) (c5 N r0 rt v0 vt a0 at)))))
+(defmulti  jerk-profile constraint-dispatch)
+(defmethod jerk-profile ::rank1
+  [N constraints] (v3/scalar-product 6 (c3 N constraints)))
+(defmethod jerk-profile ::rank2 [_N _constraints] :unimplemented)
+(defmethod jerk-profile ::rank3
+  [N constraints]
+  (v3/elem-add
+   (v3/scalar-product 6 (c3 N constraints))
+   (v3/scalar-product (* 24 N) (c4 N constraints))
+   (v3/scalar-product (* 60 N N) (c4 N constraints))))
 
-(defn accel-profile
-  ([N r0 v0 a0 rt]
-   (v3/elem-add a0 (v3/scalar-product (* 6 N) (c3 N r0 v0 a0 rt)))))
+(defmulti accel-profile constraint-dispatch)
+(defmethod accel-profile ::rank1 [N constraints]
+  (let [{a0 :a0} constraints]
+    (v3/elem-add a0 (v3/scalar-product (* 6 N) (c3 N constraints)))))
+(defmethod accel-profile ::rank2 [_N _constraints] :unimplimented)
+(defmethod accel-profile ::rank2 [_N _constraints] :unimplimented)
 
-(defn velocity-profile
-  ([N r0 v0 a0 rt]
-   (v3/elem-add
-    v0
-    (v3/scalar-product N a0)
-    (v3/scalar-product (* 3 N N) (c3 N r0 v0 a0 rt)))))
+(defmulti velocity-profile constraint-dispatch)
+(defmethod velocity-profile ::rank1
+  [N constraints]
+  (let [{v0 :v0 a0 :a0} constraints]
+    (v3/elem-add
+     v0
+     (v3/scalar-product N a0)
+     (v3/scalar-product (* 3 N N) (c3 N constraints)))))
+(defmethod velocity-profile ::rank2 [_N _constraints] :unimplimented)
+(defmethod velocity-profile ::rank3 [_N _constraints] :unimplimented)
 
 ;; TODO: This needs fixed for non-constant jerk
 (defn jerk-crit-time [N] (v3/->Vector3 N N N))
 
-(defn accel-crit-time
-  ([N r0 v0 a0 rt]
-   (->> (v3/scalar-product 6 (c3 N r0 v0 a0 rt))
-        (v3/elem3-op / (v3/scalar-product -1 a0)) (v3/elem3-op zero-does-not-exist))))
+(defmulti accel-crit-time constraint-dispatch)
+(defmethod accel-crit-time ::rank1
+  [N constraints]
+  (let [{a0 :a0} constraints]
+    (->> (v3/scalar-product 6 (c3 N constraints))
+         (v3/elem3-op / (v3/scalar-product -1 a0))
+         (v3/elem3-op zero-does-not-exist))))
+(defmethod accel-crit-time ::rank2 [_N _constraints] :unimplimented)
+(defmethod accel-crit-time ::rank3 [_N _constraints] :unimplimented)
 
 ;; TODO: This needs fixed for non-constant jerk
 (defn jerk-peak
-  ([N r0 v0 a0 rt] (jerk-profile N r0 v0 a0 rt)))
+  ([N constraints] (jerk-profile N constraints)))
 
 (defn accel-peak
-  ([N r0 v0 a0 rt]
+  ([N constraints]
    (let [{x-crit-time :x y-crit-time :y z-crit-time :z} (jerk-crit-time N)]
      (v3/->Vector3
-      (:x (accel-profile x-crit-time r0 v0 a0 rt))
-      (:y (accel-profile y-crit-time r0 v0 a0 rt))
-      (:z (accel-profile z-crit-time r0 v0 a0 rt))))))
+      (:x (accel-profile x-crit-time constraints))
+      (:y (accel-profile y-crit-time constraints))
+      (:z (accel-profile z-crit-time constraints))))))
 
 (defn vel-peak
-  ([N r0 v0 a0 rt]
-   (let [{x-crit-time :x y-crit-time :y z-crit-time :z} (accel-crit-time N r0 v0 a0 rt)]
+  ([N constraints]
+   (let [{x-crit-time :x y-crit-time :y z-crit-time :z} (accel-crit-time N constraints)]
      (v3/->Vector3
-      (:x (velocity-profile x-crit-time r0 v0 a0 rt))
-      (:y (velocity-profile y-crit-time r0 v0 a0 rt))
-      (:z (velocity-profile z-crit-time r0 v0 a0 rt))))))
+      (:x (velocity-profile x-crit-time constraints))
+      (:y (velocity-profile y-crit-time constraints))
+      (:z (velocity-profile z-crit-time constraints))))))
 
-(defn peak-values [N r0 v0 a0 rt]
-  {:peak-jerk (jerk-peak  N r0 v0 a0 rt)
-   :peak-acc  (accel-peak N r0 v0 a0 rt)
-   :peak-vel  (vel-peak   N r0 v0 a0 rt)})
+(defn peak-values [N constraints]
+  {:peak-jerk (jerk-peak  N constraints)
+   :peak-acc  (accel-peak N constraints)
+   :peak-vel  (vel-peak   N constraints)})
 
 (defrecord max-values [max-jerk max-accel max-vel])
 (defn n-mid [a b] (/ (+ a b) 2))
@@ -184,28 +191,20 @@
 (defn search-target-time
   [{max-jerk :max-jerk max-accel :max-accel max-vel :max-vel} ;; max values
    t-min t-max tol
-   {r0 :r0 v0 :v0 a0 :a0 rt :rt}] ; constraints
+   constraints] ; constraints
   (let [t-mid (n-mid t-min t-max)
-        peak-vals   (peak-values t-mid r0 v0 a0 rt)
-        max-vals    (->max-values max-jerk max-accel max-vel)
-        constraints (->constraints r0 v0 a0 rt nil nil)]
+        peak-vals   (peak-values t-mid constraints)
+        max-vals    (->max-values max-jerk max-accel max-vel)]
     (cond
       (< (- t-max t-min) tol) t-mid
       (t-is-valid? max-vals peak-vals) (search-target-time max-vals t-min t-mid tol constraints)
       :else (search-target-time max-vals t-mid t-max tol constraints))))
 
-(search-target-time {:max-jerk (v3/const 10) :max-accel (v3/const 10) :max-vel (v3/const 10)}
-                    0.01 10 0.1
-                    {:r0 (v3/->Vector3 0 0 0) :v0 (v3/->Vector3 0 0 1)
-                     :a0 (v3/->Vector3 0 0 0) :rt (v3/->Vector3 1 1 1)})
-
 (defn guidance-system
   "Projectile Guidance System (PGS)\n
      Calculates the needed Jerk to guide the projectile to the target"
   [N pos vel acc targ]
-  (jerk-profile N pos vel acc targ))
-
-(defn rand-neg1 [n] (-> (rand n) (- (/ n 2)) (* 2)))
+  (jerk-profile N {:r0 pos :v0 vel :a0 acc :rt targ}))
 
 ;; Initial Conditions
 (def target (v3/->Vector3 1 1 1))
