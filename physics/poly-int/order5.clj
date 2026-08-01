1edfa57dab2043b4b25455b6b04c3c13bf9f5067
Adam Good <dev.adamgood@gmail.com>
2026-08-01 14:18:05 -0400

    refactor: added stubs for order4-5

---
+++ physics/poly-int/order5.clj
@@ -0,0 +1,22 @@
+(load-file "vector3.clj")
+(load-file "utils.clj")
+
+(ns order5
+  (:require 
+    [vector3 :as v3]
+    [utils :refer [unimplemented]]))
+
+(defn c0 [] (unimplemented))
+(defn c1 [] (unimplemented))
+(defn c2 [] (unimplemented))
+(defn c3 [] (unimplemented))
+(defn c4 [] (unimplemented))
+(defn c5 [] (unimplemented))
+(defn jerk-profile [] (unimplemented))
+(defn accel-profile [] (unimplemented))
+(defn velocity-profile [] (unimplemented))
+(defn jerk-crit-time [] (unimplemented))
+(defn accel-crit-time [] (unimplemented))
+(defn jerk-peak [] (unimplemented))
+(defn accel-peak [] (unimplemented))
+(defn velocity-peak [] (unimplemented))
---
+++ physics/poly-int/poly-int.clj
@@ -0,0 +1,5 @@
+(load-file "order3.clj")
+(load-file "order4.clj")
+(load-file "order5.clj")
+
+(ns poly-int)
